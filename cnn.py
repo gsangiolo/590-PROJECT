450ea192f9c3c3a32556126efdbfa7926bb66803
ymiyu <yimingyu@hotmail.com>
2021-11-03 23:57:49 -0400

    update requirements

--- cnn.py
+++ cnn.py
@@ -7,12 +7,17 @@ from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStoppi
 
 def SimpleCNN(img_size, num_classes):
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), padding='same', input_shape=(img_size, img_size, 3)))
+    model.add(Conv2D(128, (3, 3), padding='same', input_shape=(img_size, img_size, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(32, (3, 3), padding='same'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
