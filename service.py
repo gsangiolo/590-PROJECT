9030b95eae9c7c0f3cd894e552be526cddab0c40
gsangiolo <sangiolo.george@gmail.com>
2021-11-29 05:27:29 +0000

    updated successfully, works now!

--- service.py
+++ service.py
@@ -3,7 +3,7 @@ from keras import models
 
 class ImagePredictor:
     def predict_image(self, image):
-        image_reshape = np.reshape(image, (np.prod(image.shape)))
+        image_reshape = np.reshape(image, (1, np.prod(image.shape)))
         print(image_reshape.shape)
         print("\n\n")
         model = models.load_model('super_simple_model')
