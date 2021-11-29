7d7b657a060f14f132b939aabcc750bd6611b412
hannakathryn <73028221+hannakathryn@users.noreply.github.com>
2021-11-29 16:24:37 -0500

    option to handle file format errors for image

--- controller.py
+++ controller.py
@@ -14,14 +14,22 @@ async def root():
     return {"message": "Hello, World!"}
 
 
+# # not sure if this will work with entering image ID
 # @app.get("/images/{image_id}")
 # async def show_image(image_id: str):
 #     full_image_id = image_id + ".jpg"
 #     return FileResponse(full_image_id)
+
+
+# # Alternative option, have the user upload the image
+# # and return predicted class
          
 
 @app.post("/predict")
 async def predict_image(image: UploadFile = File(...)):
+#     imagetype = image.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+#     if not imagetype:
+#         return "Image filetype must be .jpg, .jpeg, or .png"
     img_contents = await image.read()
     img_arr = np.fromstring(img_contents, np.uint8)
     img = cv2.imdecode(img_arr, cv2.IMREAD_COLOR)
