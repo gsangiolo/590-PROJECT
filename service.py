98ae5e4ace19b01ec0378b720616218aa98e4512
gsangiolo <sangiolo.george@gmail.com>
2021-11-29 05:02:15 +0000

    forgot to reshape image when submitting it for prediction!

--- service.py
+++ service.py
@@ -3,5 +3,8 @@ from keras import models
 
 class ImagePredictor:
     def predict_image(self, image):
+        image_reshape = np.reshape(image, (np.prod(image.shape)))
+        print(image_reshape.shape)
+        print("\n\n")
         model = models.load_model('super_simple_model')
-        return model.predict(image)
+        return model.predict(image_reshape)
