7227b4b0a9cd58cb13b1031d1d713e4cb752fd42
gsangiolo <sangiolo.george@gmail.com>
2021-11-30 00:58:55 +0000

    updated api prediction endpoints

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 import service
 import cv2
@@ -6,20 +7,36 @@ import cv2
 
 app = FastAPI()
 
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+image_service = service.ImagePredictor()
+
 @app.get("/")
 async def root():
     return {"message": "Hello, World!"}
 
+@app.get("/models-list")
+async def get_all_models():
+    return {"result": image_service.getAllModels()}
+
 @app.post("/predict")
-async def predict_image(image: UploadFile = File(...)):
+async def predict_image(model: str = Form(...), image: UploadFile = File(...)):
     img_contents = await image.read()
     img_arr = np.fromstring(img_contents, np.uint8)
     img = cv2.imdecode(img_arr, cv2.IMREAD_COLOR)
-    
-    predictor = service.ImagePredictor()
-    result = predictor.predict_image(img)
+    print("Model: ", model)
+    # predictor = service.ImagePredictor()
+    result = image_service.predict_image(img, model)
     print(result)
-    return {"result": 200}
+    return {"result": result.tolist()}
 
 #@app.post("/train-model")
 #async def train_custom_model(model_params: ModelParams):
--- service.py
+++ service.py
@@ -1,10 +1,34 @@
+import zipfile
+import os
+import shutil
 import numpy as np
 from keras import models
+from boto3 import client
+from tensorflow.python.lib.io import file_io
 
 class ImagePredictor:
-    def predict_image(self, image):
+
+    def __init__(self):
+        self.conn = client('s3', aws_access_key_id='AKIAQW4FQ6E4M74BPQXK', aws_secret_access_key='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')
+
+    def predict_image(self, image, modelName='super_simple_model'):
         image_reshape = np.reshape(image, (1, np.prod(image.shape)))
         print(image_reshape.shape)
         print("\n\n")
-        model = models.load_model('super_simple_model')
+        model = self.loadModelFromS3(modelName)
         return model.predict(image_reshape)
+
+    def getAllModels(self):
+        return [key['Key'][:-4] for key in self.conn.list_objects(Bucket='anly590-project')['Contents']]
+
+    def loadModelFromS3(self, modelName):
+        self.conn.download_file('anly590-project', modelName + '.zip', 'model.zip')
+        with zipfile.ZipFile('model.zip', 'r') as zip_ref:
+            zip_ref.extractall('model')
+        try: # Simple fix -- some models have a directory underneath, others don't!
+            model = models.load_model('model/' + modelName)
+        except:
+            model = models.load_model('model')
+        os.remove('model.zip')
+        shutil.rmtree('model')
+        return model
